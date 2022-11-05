31c0d6f986ab045532aeaf07e19c3e77dc6eb610
linguini1 <matteo.golin@gmail.com>
2022-11-05 11:20:37 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,36 @@
+# Battlesnakes competition November 5th 2022
+__author__ = "Matteo Golin & Grant Achuzia"
+
+# Imports
+from flask import Flask, request
+import json
+
+# Constants
+app = Flask(__name__)
+
+
+# Main API
+@app.route("/", methods=["GET"])
+def get():
+    return {
+        "data": "Hi, this is a test."
+    }
+
+
+@app.route("/start", methods=["POST"])
+def start():
+    return "Start"
+
+
+@app.route("/move", methods=["POST"])
+def move():
+    return "Move"
+
+
+@app.route("/end", methods=["POST"])
+def end():
+    return "End"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
